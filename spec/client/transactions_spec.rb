b12a5b1cd8c5d3c955dc8b2b000b6c652b270ad1
Eddie Huang <ehuange@gmail.com>
2023-03-13 11:15:19 -0700

    Add spec folder and files

---
+++ spec/client/transactions_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe Increase::Client::Transactions do
+  let!(:client) {
+    Increase::Client.new(
+      api_base_url: "https://sandbox.increase.com",
+      api_key: "yFj6HZkoI1qJa9EVjLVCwYNbYGjNS0KH"
+    )
+  }
+
+  context 'accounts' do
+
+  end
+end

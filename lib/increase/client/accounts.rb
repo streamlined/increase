1030046b35ad5bd652fdf3df89bae802bb17a899
Eddie Huang <ehuange@gmail.com>
2023-03-13 13:24:00 -0700

    Add tests for some accounts calls

--- lib/increase/client/accounts.rb
+++ lib/increase/client/accounts.rb
@@ -9,15 +9,17 @@ module Increase
         get("accounts/#{account_id}")
       end
 
-      def create_account(entity_id:, informational_entity_id:, name:)
+      def create_account(entity_id: nil, informational_entity_id: nil, name:)
         post("accounts", {
+              # needs to be a separate entity object (ID)
                entity_id: entity_id,
+               # an entity which does not own the account, but is associated to its activity
                informational_entity_id: informational_entity_id,
                name: name
              })
       end
 
-      def update_account(account_id:, name:)
+      def update_account(account_id:, name: nil)
         patch("accounts/#{account_id}", {
                 name: name
               })
